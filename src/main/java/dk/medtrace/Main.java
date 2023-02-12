fd2362006024209b5391b28ef2a9e897017bc817
kodekent <kodekent@kentdamgaard.dk>
2023-02-12 17:06:51 +0100

    Issue #234. Generated from the datamodel to match the database.

--- src/main/java/dk/medtrace/Main.java
+++ src/main/java/dk/medtrace/Main.java
@@ -1,7 +1,13 @@
 package dk.medtrace;
 
+import dk.medtrace.service.ToDoListService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ToDoListService service = new ToDoListService();
+
+        System.out.println("ToDoList Generator");
+
     }
 }
---
+++ src/main/java/dk/medtrace/service/ToDoListService.java
@@ -0,0 +1,19 @@
+package dk.medtrace.service;
+
+import dk.medtrace.ToDo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoListService {
+
+    List<ToDo> toDoList = new ArrayList();
+    public String asHTML() {
+        return "";
+    }
+
+    public boolean saveToDatabase() {
+        // TODO:
+        return true;
+    }
+}
