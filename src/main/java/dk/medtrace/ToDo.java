a7b2a9c3672d60b6c18b147b1311d27dc0a04d31
kodekent <kodekent@kentdamgaard.dk>
2023-02-12 17:05:53 +0100

    Adding this while they are still fucking around with the datamodel.j

---
+++ src/main/java/dk/medtrace/ToDo.java
@@ -0,0 +1,12 @@
+package dk.medtrace;
+
+
+import java.util.Date;
+
+public class ToDo {
+    Date date = new Date();
+    String text;
+    boolean isDone = false;
+
+    int priority = 2;   // Default priority
+}
